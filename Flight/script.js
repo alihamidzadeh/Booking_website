265ac3cfe2470cd0090115540b01d4bb2605d067
Arash <gholamdokht.arash@gmail.com>
2023-07-09 12:27:10 +0330

    add post method for flight page

--- Flight/script.js
+++ Flight/script.js
@@ -1,1 +1,26 @@
+var formData = {
+  from: document.getElementById("from").value,
+  to: document.getElementById("to").value,
+  departure: document.getElementById("departure").value,
+  return: document.getElementById("return").value,
+  passengers: document.getElementById("passengers").value,
+};
 
+document
+  .getElementById("flightForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    fetch("/your-backend-url", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+      });
+  });
